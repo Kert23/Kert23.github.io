14008ff13a05fd6d489befa8528eaa32f04c07ad
VasquezKhurt <72737526+Kert23@users.noreply.github.com>
2025-07-11 17:03:07 +0800

    Delete project directory

--- project/header.php
+++
@@ -1,9 +0,0 @@
-<div class="topbar">
-    <div class="branding">
-        <h1>Borrow Buddies</h1>
-    </div>
-    <div class="toggle-theme">
-        <button id="themeToggle">🌙</button>
-        <a href="logout.php" class="logout-btn">Logout</a>
-    </div>
-</div>
--- project/sidebar.php
+++
@@ -1,9 +0,0 @@
-<div class="sidebar">
-    <img src="assets/logo.png" alt="Logo" class="logo">
-    <a href="index.php"><i class="fas fa-home"></i> Dashboard</a>
-    <a href="storage.php"><i class="fas fa-box"></i> Storage</a>
-    <a href="issue.php"><i class="fas fa-hand-holding"></i> Borrow Items</a>
-    <a href="return.php"><i class="fas fa-undo"></i> Returned Items</a>
-    <a href="students.php"><i class="fas fa-users"></i> Students</a>
-    <a href="history.php"><i class="fas fa-clock"></i> Borrow History</a>
-</div>
